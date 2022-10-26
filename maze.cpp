9034a4d78c29396879902de2025d2413c70ce1f4
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-10-26 21:59:40 +0200

    added movement

--- maze.cpp
+++ maze.cpp
@@ -3,9 +3,35 @@
 
 maze::maze(std::size_t size) {
     m_size = size;
-    std::cout << m_size << std::endl;
-    //generate();
+    generate();
+    m_pos = {0, 0};
 }
 
-//void maze::generate() {
-//}
+void maze::generate() {
+    //not implemented yet so maze is hard coded
+    m_matrix = {
+        {1,1,1,1,1,0,0,1,1,1},
+        {0,1,1,1,1,1,0,1,0,1},
+        {0,0,1,0,1,1,1,0,0,1},
+        {1,0,1,1,1,0,1,1,0,1},
+        {0,0,0,1,0,0,0,1,0,1},
+        {1,0,1,1,1,0,0,1,1,0},
+        {0,0,0,0,1,0,0,1,0,1},
+        {0,1,1,1,1,1,1,1,0,0},
+        {1,1,1,1,1,0,0,1,1,1},
+        {0,0,1,0,0,1,1,0,0,1}
+    };
+    m_size = m_matrix[0].size();
+}
+
+std::pair<int, int> maze::get_pos() const {return m_pos;}
+
+void maze::move(const std::pair<int, int>& pos) {
+    std::pair<int, int> temp(m_pos.first + pos.first, m_pos.second + pos.second);
+    if (m_matrix[temp.first][temp.second]) {
+        m_pos = temp;
+    }
+    else {
+        m_message = "Couldn't move";
+    }
+}
--- maze.hpp
+++ maze.hpp
@@ -3,20 +3,22 @@
 
 #include <vector>
 #include <utility>
+#include <string>
 
 class maze {
     public:
         maze(std::size_t size);
         void print() const;
         void move(const std::pair<int, int>& position);
+        std::pair<int, int> get_pos() const;
 
     private:
         std::size_t m_size;
         std::vector<std::vector<int>> m_matrix;
         std::pair<int, int> m_pos;
+        std::string m_message;
 
         void generate();
-        std::pair<int, int> get_pos() const;
 };
 
 #endif

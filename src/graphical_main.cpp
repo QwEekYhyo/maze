76e785b915bcee76a1eed488ecedb7368d48d0ec
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-11-08 12:35:57 +0100

    changed framing system

--- src/graphical_main.cpp
+++ src/graphical_main.cpp
@@ -1,16 +1,17 @@
 #include <maze.hpp>
 
 int main(void) {
-    maze m(24);
+    maze m(30);
     std::size_t width = 1920;
     std::size_t height = 1080;
-    std::size_t frame = 0;
+    std::size_t index = 0;
+    std::size_t trigger = 30;
 
     sf::RenderWindow window(sf::VideoMode(width, height), "Maze");
 
     while (window.isOpen())
     {
-        frame++;
+        index++;
 
         sf::Event event;
         while (window.pollEvent(event))
@@ -22,15 +23,19 @@ int main(void) {
         window.clear();
         m.draw_on_window(window);
         window.display();
-        if (frame % 20 == 0) {
+        if (index >= trigger) {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
                 m.move({0, -1});
+                index = 0;
             } if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
                 m.move({0, 1});
+                index = 0;
             } if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
                 m.move({-1, 0});
+                index = 0;
             } if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
                 m.move({1, 0});
+                index = 0;
             } if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                 window.close();
             }

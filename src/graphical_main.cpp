4a483be40900cba33a8c85d5fadd595c53a85962
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-11-08 12:23:33 +0100

    added ability to move with graphical maze

--- src/graphical_main.cpp
+++ src/graphical_main.cpp
@@ -1,15 +1,17 @@
 #include <maze.hpp>
-#include <SFML/Graphics.hpp>
 
 int main(void) {
     maze m(24);
     std::size_t width = 1920;
     std::size_t height = 1080;
+    std::size_t frame = 0;
 
     sf::RenderWindow window(sf::VideoMode(width, height), "Maze");
 
     while (window.isOpen())
     {
+        frame++;
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -20,6 +22,19 @@ int main(void) {
         window.clear();
         m.draw_on_window(window);
         window.display();
+        if (frame % 20 == 0) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+                m.move({0, -1});
+            } if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+                m.move({0, 1});
+            } if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+                m.move({-1, 0});
+            } if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                m.move({1, 0});
+            } if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+                window.close();
+            }
+        }
     }
 
     return 0;
